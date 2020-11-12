f7961d01e77ba2cdaf750da7865b5eec4e1636c4
fhernanb <fhernanb@unal.edu.co>
2020-11-11 21:34:23 -0500

    mejora

--- svm/Ejemplo 01 regresion.py
+++ svm/Ejemplo 01 regresion.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Nov 11 18:16:49 2020
 
-@author: fhern
+@author: fhernanb
 """
 
 # -------------------------------------------------------------------------
@@ -14,6 +14,7 @@ Created on Wed Nov 11 18:16:49 2020
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from sklearn import svm
 
 # Los datos a usar estan disponibles en un repositorio de github
@@ -31,6 +32,8 @@ plt.title('Diagrama de dispersión con 2 variables');
 X = datos["x"]
 y = datos["y"]
 
+# El siguiente paso se debe hacer porque solo tenemos una covariable
+# en el caso de varias no es necesario usar la siguiente instrucción.
 X = X.values.reshape((-1, 1))
 
 # Creando el modelo svm con kernel lineal
@@ -45,7 +48,7 @@ mod_lin.predict([[3.0]])            # Uno por uno
 mod_lin.predict([[4.2]])            # Uno por uno
 mod_lin.predict([[3.0], [4.2]])     # Ambos
 
-# Estimando y
+# Estimando y usando los datos de entrenamiento
 y_hat_lin = mod_lin.predict(X)
 
 # Funcion para calcular MSE
@@ -57,7 +60,7 @@ mse(y, y_hat_lin)
 # Calculando el coeficiente de correlacion de Pearson
 np.corrcoef(y, y_hat_lin)
 
-# Agregando el modelo estimado
+# Agregando el modelo estimado al diagrama originar
 plt.scatter(x=datos.x, y=datos.y, color='purple', alpha=0.15)
 plt.scatter(x=datos.x, y=y_hat_lin, color="red")
 plt.xlabel('Variable X')
